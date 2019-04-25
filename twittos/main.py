5460f64d35af17d7d11c7cc595897184274be63e
RomainB <romain.boyer@polyconseil.fr>
2019-04-25 11:31:09 +0200

    Scrap in live with hashtag

--- twittos/main.py
+++ twittos/main.py
@@ -6,7 +6,16 @@ auth = tweepy.OAuthHandler(ct.CONSUMER_KEY, ct.CONSUMER_SECRET)
 auth.set_access_token(ct.ACCESS_TOKEN, ct.ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
 
-public_tweets = api.home_timeline()
-for tweet in public_tweets:
-    print(tweet.text)
-    print('- '*45)
+
+# override tweepy.StreamListener to add logic to on_status
+class MyStreamListener(tweepy.StreamListener):
+
+    def on_status(self, status):
+        print(status.text)
+        print('- ' * 30)
+
+
+myStreamListener = MyStreamListener()
+myStream = tweepy.Stream(auth=api.auth, listener=myStreamListener)
+
+myStream.filter(track=['python'])
